6e4c67f8998249c5119e072ab843f808d339102c
Hidran Arias <hidran@gmail.com>
2018-09-07 01:16:35 +0200

    Add jsx explanations, react fragment and jsx expressions

--- introapp/src/App.js
+++ introapp/src/App.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const happy =  <h2>I am happy</h2>;
+ function getDate(date) {
+    return date.toLocaleDateString()  + ' ' + date.toLocaleTimeString();
+ }
 class App extends Component {
   render() {
     return (
+      <React.Fragment>
       <div className="App">
        <h1> My firsr React App</h1>
       </div>
+      { happy }
+      { 4+4}
+      <h2> Today is  { new Date().toLocaleDateString()  + ' ' +new Date().toLocaleTimeString() }</h2>
+      <h2> { getDate(new Date())}</h2>
+      <h2> { getDate(new Date())}</h2>
+      </React.Fragment>
     );
   }
 }
--- introapp/src/index.js
+++ introapp/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+function renderApp() {
+    ReactDOM.render(<App />, document.getElementById('root'));  
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+setInterval(renderApp, 1000);
+
+
 registerServiceWorker();
