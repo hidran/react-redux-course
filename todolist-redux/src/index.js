7ed390fb9fe7e89a97097fba57bcbb6fe6fc44cb
Hidran Arias <hidran@gmail.com>
2019-02-11 09:23:48 +0100

    adds middleware logger to app

--- todolist-redux/src/index.js
+++ todolist-redux/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { createStore } from 'redux';
+import {applyMiddleware,  createStore } from 'redux';
 import storeReducer from './reducers/index';
 import {Provider} from 'react-redux';
 let storeTodos = {
@@ -36,9 +36,32 @@ let storeTodos = {
       storeTodos = currState;
      }
   }
-  
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  function logger({getState, dispatch}) {
+    console.log('MIDDLEWARE CHIAMATO');
+    return function (next) {
+            console.log('PRIMA DELLA CHIAMATA', getState());
+       return function (action) {
+           console.log('AZIONE', action)
+         console.log('PRIMA DELL\'AZIONE ', getState());
+          let result =  next(action);
+         console.log('DOPO DELLA AZIONE', getState());
+       //  console.log('RESULT', result);
+         return result;
+       }
+    }
+    
+  }
+   const logger2 = store  => next => action  => {
+    //store.dispatch({type:'LOADING'})
+     let result =  next(action);
+     console.log('RESULT2', result);
+     return result;
+   }
    const store = createStore(storeReducer, { ...storeTodos },
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() );
+       applyMiddleware(logger, logger2)
+   
+   );
 
     store.subscribe(()=>{
       const currState = JSON.stringify(store.getState());
