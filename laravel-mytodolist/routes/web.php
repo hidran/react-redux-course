fb5938e1c79d96be332c0d964fd7e49dee1b946c
Hidran Arias <hidran@gmail.com>
2019-05-20 22:17:51 +0200

    adds laravel auth scaffolding

--- laravel-mytodolist/routes/web.php
+++ laravel-mytodolist/routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', function () {
 Route::apiResource('todos', 'TodosController');
 Route::apiResource('todolists', 'TodoListsController');
 //Route::get('todos/{id}','TodosController@show');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
