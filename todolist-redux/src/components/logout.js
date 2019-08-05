b26ecf0f8cd3c0cee5862faa5763cd0077c88386
Hidran Arias <hidran@gmail.com>
2019-08-05 21:07:15 +0200

    removes Login context

--- todolist-redux/src/components/logout.js
+++ todolist-redux/src/components/logout.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Auth from '../auth/auth';
-import {LoginContext} from '../containers/logincontext'
+
 
 export default function logout(props){
-    const [user,setUser] =  React.useContext(LoginContext);
+   
     React.useEffect(()=>{
 
        Auth.logout().then(()=>{
-           setUser(null)
-        props.history.push('/login');
+          props.history.push('/login');
        });
 
     },[]);
